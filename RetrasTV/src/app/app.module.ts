c426c40d2b77a17d7b6cea68ef9e18c765a1b4dc
Valentin <valentin.barriol@ynov.com>
2019-10-18 16:11:29 +0200

    composants

--- RetrasTV/src/app/app.module.ts
+++ RetrasTV/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClipsComponent } from './clips/clips.component';
+import { StreamComponent } from './stream/stream.component';
+import { PlanningComponent } from './planning/planning.component';
+import { DescriptionComponent } from './description/description.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClipsComponent,
+    StreamComponent,
+    PlanningComponent,
+    DescriptionComponent,
+    ConnexionComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
