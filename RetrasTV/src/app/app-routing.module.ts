0c688084a7f2511f4da5ac432605bc52f002cc93
Oznerologos <lorenzo.logos@ynov.com>
2019-12-06 10:46:13 +0100

    pages

--- RetrasTV/src/app/app-routing.module.ts
+++ RetrasTV/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StreamComponent } from "./stream/stream.component";
+import { ClipsComponent } from "./clips/clips.component";
+import { PlanningComponent } from "./planning/planning.component";
+import { DescriptionComponent } from "./description/description.component";
+import { VodComponent } from "./vod/vod.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: StreamComponent },
+  { path: "clips", component: ClipsComponent },
+  { path: "planning", component: PlanningComponent },
+  { path: "description", component: DescriptionComponent },
+  { path: "vods", component: VodComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- RetrasTV/src/app/app.module.ts
+++ RetrasTV/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StreamComponent } from './stream/stream.component';
+import { PlanningComponent } from './planning/planning.component';
+import { DescriptionComponent } from './description/description.component';
+import { VodComponent } from './vod/vod.component';
+import { ClipsComponent } from './clips/clips.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StreamComponent,
+    PlanningComponent,
+    DescriptionComponent,
+    VodComponent,
+    ClipsComponent
   ],
   imports: [
     BrowserModule,
