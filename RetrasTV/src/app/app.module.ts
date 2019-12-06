24e4e5c96bbb07429a93c04aaa4439f896fd03fb
Oznerologos <lorenzo.logos@ynov.com>
2019-12-06 11:14:26 +0100

    angular material

--- RetrasTV/src/app/app.module.ts
+++ RetrasTV/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PlanningComponent } from './planning/planning.component';
 import { DescriptionComponent } from './description/description.component';
 import { VodComponent } from './vod/vod.component';
 import { ClipsComponent } from './clips/clips.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ClipsComponent } from './clips/clips.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
